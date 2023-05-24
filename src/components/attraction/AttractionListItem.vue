61f62dd5fcb1fdafeffd5ba6b1d5219246f9676e
김민석 <kmin9811@naver.com>
2023-05-24 20:00:54 +0900

    🚀 feat(deploy): build test

--- src/components/attraction/AttractionListItem.vue
+++ src/components/attraction/AttractionListItem.vue
@@ -2,10 +2,7 @@
   <div>
     <b-row
       class="m-2"
-      @click="
-        selectAttraction();
-        updateHit();
-      "
+      @click="selectAttraction"
       @mouseover="colorChange(true)"
       @mouseout="colorChange(false)"
       :class="{ 'mouse-over-bgcolor': isColor }"
@@ -20,21 +17,10 @@
           img-height="150px"
         >
           <template #header>
-            <b-row>
-              <b-col cols="8" align="left">
-                <div>
-                  <h4 class="mb-0">{{ attraction.title }}</h4>
-                </div>
-              </b-col>
-              <b-col clos="4">
-                <b-button
-                  v-on:click="addAttraction"
-                  :class="{ 'red-button': isAdded, 'green-button': !isAdded }"
-                >
-                  {{ isAdded ? "삭제" : "등록" }}
-                </b-button>
-              </b-col>
-            </b-row>
+            <div>
+              <h4 class="mb-0">{{ attraction.title }}</h4>
+              <!-- <button>등록</button> -->
+            </div>
           </template>
 
           <b-card-body>
@@ -44,14 +30,40 @@
           </b-card-body>
         </b-card>
       </div>
+      <!-- <b-card no-body class="overflow-hidden" style="height: 100px">
+      <b-row no-gutters>
+        <b-col md="6">
+          <b-card-img :src="attraction.image" alt="없음" class="rounded-0"></b-card-img>
+        </b-col>
+        <b-col md="6">
+          <b-card-body :title="attraction.title">
+            <b-card-text>
+              <hr />
+              {{ attraction.addr }}
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+      </b-row>
+    </b-card> -->
+      <!-- <b-card
+      :img-src="attraction.image"
+      img-alt="Card image"
+      img-left
+      img-width="100px"
+      class="mb-3"
+    >
+      <b-card-text> [{{ attraction.title }}] {{ attraction.addr }}] </b-card-text>
+    </b-card> -->
     </b-row>
+    <button v-on:click="addAttraction" :class="{ 'red-button': isAdded, 'green-button': !isAdded }">
+      {{ isAdded ? "삭제" : "등록" }}
+    </button>
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
 import Cookies from "js-cookie";
-import http from "@/api/http";
 
 export default {
   name: "AttractionListItem",
@@ -67,12 +79,9 @@ export default {
   methods: {
     ...mapActions(["detailAttraction"]),
     selectAttraction() {
+      // console.log("listRow : ", this.attraction);
+      // this.$store.dispatch("getAttraction", this.attraction);
       this.detailAttraction(this.attraction);
-    },
-    updateHit() {
-      http.put(`/attraction/hit/${this.attraction.contentId}`).then(() => {
-        console.log(this.attraction.title + ",,,," + this.attraction.hit);
-      });
     },
     colorChange(flag) {
       this.isColor = flag;
@@ -87,13 +96,11 @@ export default {
         let restoredSet = Cookies.get("plans");
         if (restoredSet) contentIdSet = new Set(JSON.parse(restoredSet));
         contentIdSet.add(this.attraction.contentId);
-        console.log(contentIdSet);
         Cookies.set("plans", JSON.stringify(Array.from(contentIdSet)));
       } else {
         this.isAdded ^= true;
         let contentIdSet = new Set(JSON.parse(Cookies.get("plans")));
         contentIdSet.delete(this.attraction.contentId);
-        console.log(contentIdSet);
         Cookies.set("plans", JSON.stringify(Array.from(contentIdSet)));
       }
     },
@@ -109,11 +116,9 @@ export default {
   background-color: lightblue;
 }
 .red-button {
-  border: none;
-  background-color: tomato;
+  background-color: red;
 }
 .green-button {
-  border: none;
-  background-color: mediumseagreen;
+  background-color: green;
 }
 </style>
