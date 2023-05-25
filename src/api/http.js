51c82f4302697aef945e86bbb23208d4d0e7423f
김민석 <76719828+pxxnxx@users.noreply.github.com>
2023-05-25 11:28:11 +0900

    Update http.js

--- src/api/http.js
+++ src/api/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: "http://backend:8080",
+  baseURL: "http://backend",
   headers: {
     "Content-Type": "application/json;charset=utf-8",
   },
